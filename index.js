79c36ef3d08f46905aadbab118d80db6e0be87f9
oathzsecurity <jcallis.professional@gmail.com>
2025-05-08 22:05:56 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const https = require("https");
-const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Use express.raw to get the raw body (since the ESP doesn't send proper headers for JSON parsing)
 app.use(express.raw({ type: "*/*" }));
 
 app.post("/", (req, res) => {
   console.log("✅ Received POST request");
-  console.log("🔍 Raw body:", req.body.toString());  // Convert buffer to string
-
-  res.status(200).send("👍 Received");
+  console.log("📦 Payload:", req.body.toString());
+  res.status(200).send("All good, mate.");
 });
 
 app.listen(PORT, () => {
